73dea2d5cccfd80c628dec7eadda41bcddd5e9e7
go <hingadesuraj7620@gmail.com>
2022-02-02 19:18:36 +0530

    complete Node practice

---
+++ index.js
@@ -0,0 +1,16 @@
+ const fs = require("fs");
+
+//  create folder using fs module
+//  fs.mkdirSync("pavan");
+
+//  create file and write content in file
+// fs.writeFileSync("pavan/new.txt","welcome suraj bhau");
+
+//  append data in existing file using fs module sync
+// fs.appendFileSync("pavan/new.txt","    this is a new data add in file ");
+
+// read file using fs module
+//  const readData = fs.readFileSync("pavan/new.txt");
+//  console.log(readData.toString());
+
+
