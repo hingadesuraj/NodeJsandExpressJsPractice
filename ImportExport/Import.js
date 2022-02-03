b12942c226aaf90fbe54363e493320f9d27ee21e
go <hingadesuraj7620@gmail.com>
2022-02-03 18:42:40 +0530

    Import Export file function method and variable

---
+++ ImportExport/Import.js
@@ -0,0 +1,15 @@
+// import all function from Export file and use it in import file
+const {add,sum,multiplication} = require('./Export');
+// const add = require('./Export')
+console.log(add(14,5));
+console.log(sum(140,5));
+console.log(multiplication(14,5));
+
+// // console.log("add is :");
+// add(5,10);
+// // console.log("sub is :");
+// sub(55,5);
+// // console.log("multiplication is :");
+// multiplaction(10,5);
+
+
