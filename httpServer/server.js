9e156065cd214304986409d67a6b00a107c0eab4
go <hingadesuraj7620@gmail.com>
2022-02-04 12:49:10 +0530

    add routing using node js URL module

--- httpServer/server.js
+++ httpServer/server.js
@@ -4,12 +4,25 @@ const http = require('http');
 
 //  create server using http module 
 const server = http.createServer((req,res)=>{
-    res.end("hello bro");
+    // res.end("hello bro");
+    console.log(req.url);
+    if(req.url == "/home"){
+        res.end("inside home page")
+    }else if(req.url == "/about"){
+        res.end("inside contact us page");
+    }else if(req.url == "/services"){
+        // res.write("inside in service page");
+        // res.sendDate();
+        res.end("inside services page");
+    }else{
+        res.writeHead(404,{"Content-Type":"text/html"}) // send status code with content type html 
+        res.end("<h1>404 page not found</h1>");
+    }
 })
 
 
 
 // listing server on port 8000 
-server.listen(8000,"127.0.0.1",(req,res)=>{
+server.listen(8000,"127.0.0.1",()=>{
     console.log("server running on port 8000");
 })
